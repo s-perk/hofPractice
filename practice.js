1b1021d1c52b808c576c13826b175f9d2a1adaf8
Stephen Perkins <stperk21@gmail.com>
2023-01-11 10:29:27 -0500

    pass through movies in _.reduce()

--- practice.js
+++ practice.js
@@ -83,18 +83,35 @@ var startsWith = function (fruits, letter) {
       resultString = resultString + ' ' + item;
     }
   });
-  resultString = resultString.slice(1,resultString.length);
+  resultString = resultString.slice(1, resultString.length);
   return resultString.split(' ');
 };
 
 // return a filtered array containing only cookie-type desserts.
+// cannot create new arrays
 var cookiesOnly = function (desserts) {
 
+  // Call _.filter on desserts
+  // Returning each item pushes it onto a new array, so we can create a new array here
+  // without needing to initialize a separate array
+  desserts = _.filter(desserts, function (item) {
+    if (item.type === 'cookie') {
+      return item;
+    } 
+  });
+
+  return desserts;
 };
 
 // rebuild the getUserTweets function from above with _.filter instead
 var filterUserTweets = function(tweets, user) {
 
+  tweets = _.filter(tweets, function (tweet) {
+    if (tweet['user'] === user) {
+      return tweet.message;
+    }
+  });
+  return tweets;
 };
 
 /*
@@ -106,6 +123,11 @@ var filterUserTweets = function(tweets, user) {
 // given an array of strings, use _.map to return a new array containing all
 // strings converted to uppercase letters.
 var upperCaseFruits = function (fruits) {
+  fruits = _.map(fruits, function (item) {
+    return item.toUpperCase();
+  });
+
+  return fruits;
 
 };
 
@@ -113,13 +135,24 @@ var upperCaseFruits = function (fruits) {
 // that have a new "glutenFree" property, with a boolean value.
 // TIP: Items that contain flour are not gluten-free.
 var glutenFree = function (desserts) {
+  _.map(desserts, function (dessert) {
+    if (dessert.ingredients.indexOf('flour') > -1) {
+      dessert.glutenFree = false;
+    } else {
+      dessert.glutenFree = true;
+    }
+  });
 
+  return desserts;
 };
 
 // given an array of tweet objects, return a new array of strings
 // containing only the message properties.
 var allUserMessages = function(tweets) {
 
+  return _.map(tweets, function (item) {
+    return item.message;
+  });
 };
 
 // use _.map to return an array of items with their sale prices, with a new property
@@ -143,7 +176,11 @@ var allUserMessages = function(tweets) {
 
 */
 var applyCoupon = function (groceries, coupon) {
-
+  return _.map(groceries, function (item) {
+    price = +item.price.slice(1, item.price.length);
+    item.salePrice = '$' + (price - (price * coupon)).toFixed(2);
+    return item;
+  });
 };
 
 /*
@@ -155,12 +192,28 @@ var applyCoupon = function (groceries, coupon) {
 // return the total price of all products.
 var sumTotal = function (products) {
 
+  return _.reduce(products, function (memo, item) {
+    price = +item.price.slice(1, item.price.length);
+    
+    return memo + price;
+  }, 0);
+
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function (desserts) {
 
+  return _.reduce(desserts, function(memo, item) {
+    dessertType = item.type;
+    if (memo[dessertType] !== undefined) {
+      memo[dessertType] += 1;
+    } else {
+      memo[dessertType] = 1;
+    }
+    return memo;
+
+  }, {});
 };
 
 // return an object with the proper count of all user messages
@@ -175,6 +228,17 @@ var dessertCategories = function (desserts) {
   }
 */
 var countMessagesPerUser = function(tweets) {
+  return _.reduce(tweets, function(memo, item) {
+    user = item['user'];
+    
+    if (memo[user] !== undefined) {
+      memo[user] += 1;
+    } else {
+      memo[user] = 1;
+    }
+    
+    return memo;
+  }, {});
 
 };
 
